8bedc1ec6d032ca7976dd1f131af1c44c99b12bb
jjaram91 <89406470+jjaram91@users.noreply.github.com>
2021-08-23 18:00:44 +0200

    Add files via upload

---
+++ Schleifen.py
@@ -0,0 +1,24 @@
+n = 1
+while n <= 10:
+    print (n * n)
+    n = n + 1
+    
+print("\n")  
+
+n = 1
+for n in range(1, 11):
+    print (n * n)
+    n += 1
+    
+print("\n")   
+ 
+Liste = ["Hamburg", "Berlin", "Stuttgart", "Bonn", "Hannover", "Köln", "Dresden", "Leipzig", "Kiel", "München"]
+s = 0
+while s < len(Liste):
+    print (Liste[s])
+    s += 1
+    
+staedte = ["Hamburg", "Berlin", "Stuttgart", "Bonn", "Hannover", "Köln", "Dresden", "Leipzig", "Kiel", "München"]
+for stadt in staedte:
+    print (stadt)
+    
